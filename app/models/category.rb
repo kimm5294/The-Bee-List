07b050378aa4e95a3013c6af1b52d02a374dd412
Matthew Kim <kimm5294@gmail.com>
2017-05-18 19:31:22 -0400

    create method display user goals for displaying users goals within a category

--- app/models/category.rb
+++ app/models/category.rb
@@ -6,4 +6,8 @@ class Category < ApplicationRecord
   def top_five_goals
     self.goals.sort_by{|goal| goal.users_count}.reverse[0...5]
   end
+
+  def display_user_goals(user)
+    self.goals.joins(:goals_users).where(goals_users: {user_id: user.id})
+  end
 end
