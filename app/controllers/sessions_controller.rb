c65a7fa66b7cdeb763555e872a6c6f51ad27dd86
Gurkanver Brar & Slip <pair+gbrar.slipnslide@devbootcamp.com>
2017-05-18 16:06:30 -0400

    created sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:session][:email])
+
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user), notice: "You have successfully logged in!"
+    else
+      flash.now[:error] = "Invalid username or password"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = @current_user = nil
+    redirect_to root_url
+  end
+end
