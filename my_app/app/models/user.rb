0f1fb8e35972f050a85fe1adf33a5e492dfa027d
Bryan Mateer <email": null>
2017-05-18 11:17:41 -0400

    Add user associations

--- my_app/app/models/user.rb
+++ my_app/app/models/user.rb
@@ -1,7 +1,32 @@
 class User < ApplicationRecord
+  has_secure_password
   validates :first_name, :last_name, :email, :username, presence: true
   validates :email, :username, uniqueness: true
   validates :password, length: {minimum: 6}
   validates_confirmation_of :password
-  
+
+  has_many :connections_as_friender, class_name: "Connection", foreign_key: "friender_id"
+  has_many :connections_as_friendee, class_name: "Connection", foreign_key: "friendee_id"
+  has_many :friendees, through: :connections_as_friender
+  has_many :frienders, through: :connections_as_friendee
+  has_many :goalsusers
+  has_many :goals, through: :goalsusers
+
+  def friends
+    self.frienders + self.friendees
+  end
+
+  def is_friends_with?(friendee)
+    !!connection(friendee)
+  end
+
+  def connection(friendee)
+    if !!Connection.find_by(friender_id: self.id, friendee_id: friendee.id)
+      return Connection.find_by(friender_id: self.id, friendee_id: friendee.id)
+    elsif !!Connection.find_by(friender_id: friendee.id, friendee_id: self.id)
+      return Connection.find_by(friender_id: friendee.id, friendee_id: self.id)
+    else
+      return nil
+    end
+  end
 end
