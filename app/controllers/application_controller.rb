51a6b1e4b88e0f70618ef01c7b1c3e2c9e468120
Gurkanver Brar & Slip <pair+gbrar.slipnslide@devbootcamp.com>
2017-05-18 16:05:50 -0400

    added helpers to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    current_user != nil
+  end
 end
