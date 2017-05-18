30ed655707da2e8b869cea234c658afc55b35e61
Gurkanver Brar & Slip <pair+gbrar.slipnslide@devbootcamp.com>
2017-05-18 16:35:20 -0400

    added friends method to model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,6 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:username, :email, :password, :password_confirmation)
+      params.require(:user).permit(:first_name, :last_name, :username, :email, :password, :password_confirmation)
     end
 end
