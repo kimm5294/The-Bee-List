d7c6019d4203255d1c53c51910974a906e9568bd
Bryan Mateer <email": null>
2017-05-18 11:23:29 -0400

    Add associations for goal

--- my_app/app/models/goal.rb
+++ my_app/app/models/goal.rb
@@ -1,4 +1,8 @@
 class Goal < ApplicationRecord
   validates :task, :category_id, presence: true
   validates :task, uniqueness: true
+
+  has_many :goalsusers
+  has_many :users, through: :goalsusers
+  has_many :categories
 end
