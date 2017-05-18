34759b4987d895d65671060c0bf8b1683ec29cc9
Bryan Mateer <email": null>
2017-05-18 17:21:59 -0400

    Create route for creating a user's goal

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :categories, only: [:index, :show]
+  resources :goals_users, only: [:create]
   root 'homepage#index'
   resources :sessions, only: [:new, :create, :destroy]
   resources :connections, only: [:create, :destroy]
