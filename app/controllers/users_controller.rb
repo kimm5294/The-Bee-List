a4d128033d28dbd26ff15af70bcbeba615570b54
Matthew Kim <kimm5294@gmail.com>
2017-05-19 11:57:16 -0400

    add case statement to search apis for movies and tv series

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,39 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:user_id])
   end
 
+  def search
+    @category = Category.find_by_id(params[:cat_id])
+
+    case @category.id
+    when 1
+      search_params = OMDB.search(params["search"])
+      result = OMDB.search(params["search"]).select{|film| film.type=="movie"}
+      if search_params.class != Array || result.length ==0
+        @errors= ["No results found"]
+      else
+        @results= result
+      end
+    when 2
+      search_params = OMDB.search(params["search"])
+      result = OMDB.search(params["search"]).select{|film| film.type=="series"}
+      puts result
+      if search_params.class != Array || result.length ==0
+        @errors= ["No results found"]
+      else
+        @results = result
+      end
+    when 3
+
+
+    when 4
+
+    when 5
+
+    when 6
+
+    end
+  end
+
   private
     def user_params
       params.require(:user).permit(:first_name, :last_name, :username, :email, :password, :password_confirmation)
