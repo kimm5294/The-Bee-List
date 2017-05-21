ea815fcd8dce1c40faa44d23a33235b0dbcef2a5
Adriel Saporta <email": null>
2017-05-21 14:00:58 -0400

    Alphabetize routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :categories, only: [:index, :show]
-  resources :goals_users, only: [:create, :destroy, :update]
   root 'homepage#index'
-  resources :goals, only: [:show, :create]
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :categories, only: [:index, :show]
   resources :connections, only: [:create, :destroy]
+  resources :goals, only: [:show, :create]
+  resources :goals_users, only: [:create, :destroy, :update]
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:destroy, :create, :update]
   resources :users do
     get "/friends" => "users#friends"
