29cad9d3545ab4a2a85d316006239ef4be306253
Gurkanver Brar <Brargurkanver@gmail.com>
2017-05-21 18:01:23 -0400

    chat messengers, DO NOT MERGE PR, still needs work

--- app/controllers/conversation_controller.rb
+++ app/controllers/conversation_controller.rb
@@ -1,5 +1,5 @@
 class ConversationController < ApplicationController
-  before_action :authenticate_user
+  # before_action :authenticate_user
 
   def index
     @users = User.all
--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -5,8 +5,8 @@ class Conversation < ApplicationRecord
   has_many :messages, dependent: :destroy
 
   validates_uniqueness_of :sender_id, :scope => :recipient_id
-  
+
   scope :between, -> (sender_id,recipient_id) do
-  where(“(conversations.sender_id = ? AND conversations.recipient_id =?) OR (conversations.sender_id = ? AND conversations.recipient_id =?)”, sender_id,recipient_id, recipient_id, sender_id)
-  end
+   where(“(conversations.sender_id = ? AND conversations.recipient_id = ? ) OR (conversations.sender_id = ? AND conversations.recipient_id =?)”, sender_id,recipient_id, recipient_id, sender_id)
+   end
 end
