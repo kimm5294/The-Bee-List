d7deb38f48bda8897a5b947ed3de6257f245c35c
Gurkanver Brar <Brargurkanver@gmail.com>
2017-05-22 17:07:21 -0400

    show name of person the conversation is with, changed to message only friends

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,8 +1,9 @@
 class ConversationsController < ApplicationController
   before_action :authenticate_user
-  
+
   def index
    @users = User.all
+   @user = current_user
    @conversations = Conversation.all
    end
 
