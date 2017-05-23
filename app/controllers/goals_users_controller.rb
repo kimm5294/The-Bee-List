e89e9d7490aadedbf577ccd6e5ef1f3493c460b2
Matthew Kim <kimm5294@gmail.com>
2017-05-23 10:57:17 -0400

    change redirect for goals users create to go to current user id instead of params

--- app/controllers/goals_users_controller.rb
+++ app/controllers/goals_users_controller.rb
@@ -2,7 +2,7 @@ class GoalsUsersController < ApplicationController
 
   def create
     goal = GoalsUser.create(user_id: current_user.id, goal_id: params[:goal_id])
-    redirect_to "/users/#{params[:user_id]}"
+    redirect_to "/users/#{current_user.id}"
   end
 
   def update
