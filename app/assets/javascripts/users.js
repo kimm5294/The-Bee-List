b9ea9b679022e70f3232ea44de190f806bc4f940
Matthew Kim <kimm5294@gmail.com>
2017-05-23 18:07:10 -0400

    add ajax for searching for other users

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -18,14 +18,37 @@ $(function(){
   $('#profiletabs ul li a').on('click', function(e){
     e.preventDefault();
     var newcontent = $(this).attr('href');
-    
+
     $('#profiletabs ul li a').removeClass('sel');
     $(this).addClass('sel');
-    
+
     $('#content section').each(function(){
       if(!$(this).hasClass('hidden')) { $(this).addClass('hidden'); }
     });
-    
+
     $(newcontent).removeClass('hidden');
   });
+
+  $(".friends-search-form").on("submit", function(event) {
+    event.preventDefault();
+
+    var data = $(this).serialize();
+    var url = $(this).attr("action")
+    var method = $(this).attr("method")
+
+    $.ajax({
+      url: url,
+      method: method,
+      data: data
+    })
+    .done(function(response) {
+      $(".friends-search-results-div").html(response);
+    })
+  })
+
+
+
+
+
+
 });
